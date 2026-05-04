bf5c5043ac8f6b7170aa81ae7c8f43790c956e95
Cimetric <paulgaudin96@gmail.com>
2026-05-04 12:56:05 -0600

    Fix colors db prePOP.

--- colors_db.sql
+++ colors_db.sql
@@ -1,16 +1,19 @@
-CREATE TABLE colors (id INT, name VARCHAR(255), hex_value VARCHAR(255));
+CREATE TABLE colors (
+    id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL, 
+    name VARCHAR(255) UNIQUE NOT NULL, 
+    hex_value VARCHAR(7) UNIQUE NOT NULL);
 
 INSERT INTO colors (id, name, GPA) VALUES 
-(800000001, 'Red', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000002, 'Orange', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000003, 'Yellow', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000004, 'Green', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000005, 'Blue', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000006, 'Purple', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000007, 'Grey', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000008, 'Brown', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000009, 'Black', ROUND(2.0 + (RAND() * 2.0), 2)),
-(800000009, 'Teal', ROUND(2.0 + (RAND() * 2.0), 2));
+('Red', '#FF0000'),
+('Orange', '#FFA500'),
+('Yellow', '#FFFF00'),
+('Green', '#008000'),
+('Blue', '#0000FF'),
+('Purple', '#800080'),
+('Grey', '#808080'),
+('Brown', '#A52A2A'),
+('Black', '#000000'),
+('Teal', '#008080');
 
 SHOW CREATE TABLE colors;
 
